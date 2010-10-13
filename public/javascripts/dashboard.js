e2298a6a151259306122877abc65c5f0f548eac7
Nicolas Blanco <slainer68@gmail.com>
2010-10-13 23:46:40 +0200

    Enhancements.

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,9 +1,8 @@
-jQuery.noConflict();
-jQuery(document).ready(function() {
-   jQuery('.property_selector span').click(function(){ 
-	     jQuery(this).parent('div.property_selector').toggleClass('hover');
-   });
-   jQuery('.container, .navbar').mouseup(function(){ 
-	     jQuery('.top .property_selector').removeClass('hover');
-   });
+$(document).ready(function() {
+  $('.property_selector span').click(function(){ 
+    $(this).parent('div.property_selector').toggleClass('hover');
+  });
+  $('.container, .navbar').mouseup(function(){ 
+    $('.top .property_selector').removeClass('hover');
+  });
 });
