aee62dedae34916585abed2f6da73f43c126535c
Nicolas Blanco <slainer68@gmail.com>
2010-10-19 02:27:47 +0200

    rescue documentnotfound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from Mongoid::Errors::DocumentNotFound, :with => :not_found
+  
+  protected
+  def not_found
+    render :template => Rails.root.join("public", "404.html"), :layout => false, :status => :not_found
+  end
 end
