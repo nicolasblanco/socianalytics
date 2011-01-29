10ecdf303f5e55fa06a06b2b32ff79acb4a558d5
Nicolas Blanco <slainer68@gmail.com>
2011-01-29 22:58:29 +0100

    Finally implementing the first chart :)

--- app/jobs/twitter_user_job.rb
+++ app/jobs/twitter_user_job.rb
@@ -4,19 +4,23 @@ class TwitterUserJob
   extend Resque::Plugins::Restriction
   
   @queue = :normal
-  restrict :per_hour => 200, :concurrent => 1
+  restrict :per_hour => 300, :concurrent => 1
   
-  def self.restriction_identifier(user_id, twitter_id)
+  def self.restriction_identifier(user_id, twitter_id, additional_data)
     [self.to_s, twitter_id].join(":")
   end
   
-  def self.perform(user_id, twitter_id)
+  def self.perform(user_id, twitter_id, additional_data)
     user = User.find(user_id)
     twitter_user = TwitterUser.find_or_initialize_by(:twitter_id => twitter_id)
     
     twitter_user.raw = user.twitter_client.user(:user_id => twitter_id)
-    twitter_user.followers_ids  = user.twitter_client.follower_ids(:user_id => twitter_id).ids
-    twitter_user.followings_ids = user.twitter_client.friend_ids(:user_id => twitter_id).ids
+    
+    if additional_data
+      twitter_user.followers_ids  = user.twitter_client.follower_ids(:user_id => twitter_id).ids
+      twitter_user.followings_ids = user.twitter_client.friend_ids(:user_id => twitter_id).ids
+    end
+    
     twitter_user.save
   end
 end
--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -1,15 +1,26 @@
 class TwitterUser
   include Mongoid::Document
-  include Mongoid::Timestamps
   include Mongoid::Versioning
+  include Mongoid::Timestamps
   
-  field :raw, :type => Hash
+  @@twitter_fields = %w(location profile_image_url screen_name friends_count followers_count)
+  cattr_reader :twitter_fields
+  
   field :twitter_id
   field :followers_ids,  :type => Array
   field :followings_ids, :type => Array
+  @@twitter_fields.each { |f| field :"twitter_#{f}" }
+  
+  def raw=(twitter_response)
+    twitter_fields.each { |tf| send("twitter_#{tf}=", twitter_response.send(tf)) if twitter_response.include?(tf) }
+  end
   
   def friends_ids
     followers_ids & followings_ids
+  end
+  
+  def friends_size
+    friends_ids.size
   end
   
   def async_update!(user)
@@ -17,10 +28,18 @@ class TwitterUser
   end
   
   def update_associated_with!(user, force = false)
-    puts "update_associated_with! for #{twitter_id}, recursive = #{recursive}"
+    puts "update_associated_with! for #{twitter_id}, force = #{force}"
     (followers_ids | followings_ids).each do |current_twitter_id|
       current_twitter_user = TwitterUser.where(:twitter_id => current_twitter_id).first
-      Resque.enqueue(TwitterUserJob, user.id, current_twitter_id) if force || current_twitter_user.blank? || current_twitter_user.updated_at < 3.days.ago
+      Resque.enqueue(TwitterUserJob, user.id, current_twitter_id, false) if force || current_twitter_user.blank? || current_twitter_user.updated_at < 1.day.ago
     end
+  end
+  
+  def stat_mash(attribute)
+    Hashie::Mash.new(:updated_at => updated_at, :value => send(attribute))
+  end
+  
+  def stat_for(attribute)
+    versions.map { |v| v.stat_mash(attribute) }.push(stat_mash(attribute))
   end
 end
