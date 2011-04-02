2bc6403405b81279e6820c4a8ee84de67e720431
slainer68 <slainer68@gmail.com>
2011-04-03 00:06:48 +0200

    changes cookie key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Socianalytics::Application.config.session_store :cookie_store, :key => '_toptweet_session'
+Socianalytics::Application.config.session_store :cookie_store, :key => '_socianalytics_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# Transmediatic::Application.config.session_store :active_record_store
+# Socianalytics::Application.config.session_store :active_record_store
